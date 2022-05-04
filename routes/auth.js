48a455223e960013516d5d6590cc928d23cfeb11
sammiepius <sammiepius10@gmail.com>
2022-05-04 10:35:58 +0100

    added functions

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 //REGISTER
 
@@ -39,14 +40,18 @@ router.post('/login', async (req, res) => {
     OriginalPassword !== req.body.password &&
       res.status(401).json('wrong credentials!!!!');
 
-    const accessToken = jwt.sign({
-      id: user._id,
-      isAdmin: user.isAdmin,
-    });
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: '3d' }
+    );
 
     const { password, ...others } = user._doc;
 
-    res.status(200).json(others);
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,76 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require('./verifyToken');
+
+//UPDATE
+
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+
+router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json('user has been deleted....');
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+//GET USER
+
+router.get('/find/:id', verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ others });
+  } catch (err) {
+    res.json(err);
+  }
+});
+//GET ALL USER
+
+router.get('/', verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+//GET ALL STAT
+
+
+module.exports = router;
 
 // router.get('/usertest', (req, res) => {
 //   res.send('user test is successful');
@@ -9,4 +81,3 @@ const router = require('express').Router();
 //   console.log(username);
 //   res.send('your username:' + username);
 // });
-module.exports = router;
